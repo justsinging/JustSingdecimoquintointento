5e7f28c0a53146642cc9bd13abd73c5a42544f0a
justsinging <jbarnes16103@gmail.com>
2025-06-16 16:25:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -33,21 +33,45 @@
 const productos = [
   {
     id: 1,
-    nombre: "Bolsa 001",
-    precio: 4500,
-    imagen: "ruta/de/tu/imagen1.jpg"
+    nombre: "Bolsa Mujer",
+    precio: 16500,
+    imagen: "https://i.imgur.com/bMZdpKo.jpeg "
   },
   {
     id: 2,
-    nombre: "Bolsa 002",
-    precio: 4700,
-    imagen: "ruta/de/tu/imagen2.jpg"
+    nombre: "Bolsa Mida",
+    precio: 18500,
+    imagen: "https://i.imgur.com/RTeouUR.jpeg"
   },
   {
     id: 3,
-    nombre: "Bolsa 003",
-    precio: 4900,
-    imagen: "ruta/de/tu/imagen3.jpg"
+    nombre: "Bolsa 13",
+    precio: 13000,
+    imagen: "https://i.imgur.com/BTUjfH7.jpeg"
+  }
+  {
+    id: 4,
+    nombre: "Bolsa Lavanda",
+    precio: 16500,
+    imagen: "https://i.imgur.com/B6E9D7j.jpeg"
+  }
+ {
+    id: 5,
+    nombre: "Bolsa Florero",
+    precio: 20000,
+    imagen: "https://i.imgur.com/MTxPBeC.jpeg"
+  }
+ {
+    id: 6,
+    nombre: "Bolsa Olivo",
+    precio: 13000,
+    imagen: "https://i.imgur.com/z1EjOsG.jpeg"
+  }
+ {
+    id: 7,
+    nombre: "Bolsa Frutis",
+    precio: 13000,
+    imagen: "https://i.imgur.com/z1EjOsG.jpeg"
   }
 ];
 
